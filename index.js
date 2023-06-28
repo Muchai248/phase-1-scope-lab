9caf05fd91713a4af3b1bd134d31079e350c9034
Mark Muchai <muchaimark6@gmail.com>
2023-06-28 11:41:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return(customerName);
+
+}
+function setBestCustomer(){
+    bestCustomer="not bob";
+    return(bestCustomer);
+}
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+   
+}
+
+const leastFavoriteCustomer = "initial value";
+
+function  changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="maybe bob";
+}
+
+
+
+
+
